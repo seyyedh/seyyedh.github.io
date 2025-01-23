a2dc84e1ad3705d0e1012a038f3a09b45c2c074d
seyyedh <syajadda@yahoo.com>
2025-01-23 15:13:22 +0000

    Deploying to gh-pages from @ seyyedh/seyyedh.github.io@da5975626ab35638c7f9736c0e473312951888a7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,8 +431,8 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+    },{id: "news-publishing-my-article-in-the-archive",
+          title: 'Publishing my article in the archive',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
